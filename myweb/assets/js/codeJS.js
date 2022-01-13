f29c8aef2019cd49474c5d02e60b8891534d20f3
ObaidMed <96299663+ObaidMed@users.noreply.github.com>
2022-01-13 23:13:27 +0100

    Origin Files

---
+++ myweb/assets/js/codeJS.js
@@ -0,0 +1,130 @@
+function calculateAmount(val) {  
+    var tot_price = val * 500;  
+    var divobj = document.getElementById('tot_amount');  
+    divobj.value = tot_price;  
+}  
+
+  
+
+
+/** */
+
+function calc1() {
+    var Text = document.getElementById("ca1").value;
+    var price = 1300 ;
+    var re = (price * Text)
+    window.counter.innerHTML = re;
+   
+
+}
+
+function calc2() {
+    var Text = document.getElementById("ca2").value;
+    var price = 1100;
+    var re = (price * Text)
+    window.counter.innerHTML = re;
+
+}
+function calc3() {
+    var Text = document.getElementById("ca3").value;
+    var price = 1050;
+    var re = (price * Text)
+    window.counter.innerHTML = re;
+
+}
+function calc4() {
+    var Text = document.getElementById("ca4").value;
+    var price = 1000;
+    var re = (price * Text)
+    window.counter.innerHTML = re;
+
+}
+function calc5() {
+    var Text = document.getElementById("ca5").value;
+    var price = 1050;
+    var re = (price * Text)
+    window.counter.innerHTML = re;
+
+}
+
+  /*b2*/ 
+  function calc11() {
+    var Text = document.getElementById("ca11").value;
+    var price = 300 ;
+    var re = (price * Text)
+    window.counter.innerHTML = re;
+
+} 
+function calc12() {
+    var Text = document.getElementById("ca12").value;
+    var price = 300 ;
+    var re = (price * Text)
+    window.counter.innerHTML = re;
+
+} 
+function calc13() {
+    var Text = document.getElementById("ca13").value;
+    var price = 300 ;
+    var re = (price * Text)
+    window.counter.innerHTML = re;
+
+} 
+function calc14() {
+    var Text = document.getElementById("ca14").value;
+    var price = 300 ;
+    var re = (price * Text)
+    window.counter.innerHTML = re;
+
+} 
+function calc15() {
+    var Text = document.getElementById("ca15").value;
+    var price = 300 ;
+    var re = (price * Text)
+    window.counter.innerHTML = re;
+
+} 
+
+  /*b3*/ 
+  function calc16() {
+    var Text = document.getElementById("ca16").value;
+    var price = 300 ;
+    var re = (price * Text)
+    window.counter.innerHTML = re;
+    
+} 
+
+function calc17() {
+    var Text = document.getElementById("ca17").value;
+    var price = 300 ;
+    var re = (price * Text)
+    window.counter.innerHTML = re;
+    
+} 
+
+function calc18() {
+    var Text = document.getElementById("ca18").value;
+    var price = 300 ;
+    var re = (price * Text)
+    window.counter.innerHTML = re;
+    
+} 
+function calc19() {
+    var Text = document.getElementById("ca19").value;
+    var price = 300 ;
+    var re = (price * Text)
+    window.counter.innerHTML = re;
+    
+} 
+function calc20() {
+    var Text = document.getElementById("ca20").value;
+    var price = 300 ;
+    var re = (price * Text)
+    window.counter.innerHTML = re;
+    
+} 
+
+
+
+
+
+
